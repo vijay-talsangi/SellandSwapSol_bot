2dd6a0098ecd4d676e63d88cd1d6236932b56983
vijay-talsangi <vijaytalsangi4705@gmail.com>
2025-09-26 13:06:49 +0530

    Created wallet for users

--- index.ts
+++ index.ts
@@ -1,9 +1,34 @@
-import { Telegraf } from 'telegraf'
-import { message } from 'telegraf/filters'
+import { Telegraf } from 'telegraf';
+import { message } from 'telegraf/filters';
+import { PrismaClient } from './generated/prisma';
+import { Keypair } from '@solana/web3.js';
 
-const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN!)
+const prismaClient = new PrismaClient();
+const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN!);
 
-bot.start((ctx) => {
-    ctx.reply('Welcome to the SellandSwapSol Bot')
-})
-bot.launch()
+bot.start(async (ctx) => {
+    const existingUser = await prismaClient.users.findFirst({
+        where: { 
+            tgUserId: ctx.chat.id.toString()
+        }
+    });
+    
+    if(existingUser){
+        const publicKey = existingUser.publicKey;
+        ctx.reply(`Welcome to the SellandSwapSol Bot. Here is your public key: ${publicKey}`);
+
+    } else {
+        const keypair = Keypair.generate();
+        await prismaClient.users.create({
+            data: {
+                tgUserId: ctx.chat.id.toString(),
+                publicKey: keypair.publicKey.toBase58(),
+                privateKey: keypair.secretKey.toString()
+            }
+        })
+        const publicKey = keypair.publicKey.toString();
+        ctx.reply(`Welcome to the SellandSwapSol Bot. Here is your public key: ${publicKey}
+        You can trade on solana now, first put some sol here.`);
+    }
+});
+bot.launch();
