08d8594d57d0fbd76f8194cfb627f1554d705da4
vijay-talsangi <vijaytalsangi4705@gmail.com>
2025-09-26 14:46:12 +0530

    Added Buttons to show public and private key

--- index.ts
+++ index.ts
@@ -1,10 +1,15 @@
-import { Telegraf } from 'telegraf';
-import { message } from 'telegraf/filters';
+import { Telegraf, Markup } from 'telegraf';
+// import { message } from 'telegraf/filters';
 import { PrismaClient } from './generated/prisma';
 import { Keypair } from '@solana/web3.js';
 
 const prismaClient = new PrismaClient();
 const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN!);
+
+const DEFAULT_KEYBOARD = Markup.inlineKeyboard([
+    Markup.button.callback("Show public key", "public_key"),
+    Markup.button.callback("Show Private key", "private_key"),
+]);
 
 bot.start(async (ctx) => {
     const existingUser = await prismaClient.users.findFirst({
@@ -14,8 +19,9 @@ bot.start(async (ctx) => {
     });
     
     if(existingUser){
-        const publicKey = existingUser.publicKey;
-        ctx.reply(`Welcome to the SellandSwapSol Bot. Here is your public key: ${publicKey}`);
+        ctx.reply(`Welcome back to the SellandSwapSol Bot.`, {
+            ...DEFAULT_KEYBOARD
+        });
 
     } else {
         const keypair = Keypair.generate();
@@ -23,12 +29,43 @@ bot.start(async (ctx) => {
             data: {
                 tgUserId: ctx.chat.id.toString(),
                 publicKey: keypair.publicKey.toBase58(),
-                privateKey: keypair.secretKey.toString()
+                privateKey: keypair.secretKey.toBase64()
             }
         })
         const publicKey = keypair.publicKey.toString();
-        ctx.reply(`Welcome to the SellandSwapSol Bot. Here is your public key: ${publicKey}
-        You can trade on solana now, first put some sol here.`);
+        ctx.reply(`Welcome to the SellandSwapSol Bot.
+            You can trade on solana now, first put some sol here.`, {
+            ...DEFAULT_KEYBOARD
+        });
     }
 });
+
+bot.action("public_key", async (ctx) => {
+    await ctx.answerCbQuery();
+    const user = await prismaClient.users.findFirst({
+        where: { 
+            tgUserId: ctx.chat?.id.toString()
+        }
+    });
+    return ctx.reply(
+        `Your public key is ${user?.publicKey}`, {
+            ...DEFAULT_KEYBOARD
+        }
+    );
+});
+
+bot.action("private_key", async (ctx) => {
+    await ctx.answerCbQuery();
+    const user = await prismaClient.users.findFirst({
+        where: { 
+            tgUserId: ctx.chat?.id.toString()
+        }
+    });
+    return ctx.reply(
+        `Your private key is ${user?.privateKey}`, {
+            ...DEFAULT_KEYBOARD
+        }
+    );
+});
+
 bot.launch();
